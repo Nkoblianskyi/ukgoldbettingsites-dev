b1867b5bf50ac9cfef8806a799b4f235f595577e
Nykyta Koblianskyi <nkoblianskyi@gmail.com>
2026-05-13 11:02:12 +0200

    5

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,6 @@ import { Footer } from "@/components/footer"
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-background">
-      <TopSitesModal />
       <Header />
       <Hero />
       <RatingSection />
@@ -19,7 +18,6 @@ export default function HomePage() {
       <CookieBanner />
       <TopSitesModal />
       <Footer />
-
     </div>
   )
 }
